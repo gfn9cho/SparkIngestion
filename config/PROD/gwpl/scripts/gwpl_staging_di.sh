d2a5cd5fe2a5bfb7687a1765fef220120c8e882f
Prabha <prabhakar.chockalingam@stateauto.com>
2020-03-22 18:17:04 -0400

    Added Staging Config files for new PROD cluster

--- config/PROD/gwpl/scripts/gwpl_staging_di.sh
+++ config/PROD/gwpl/scripts/gwpl_staging_di.sh
@@ -1,44 +1,29 @@
 #!/bin/bash
 
 
-echo "Started the script for GWPL DI Staging "
-base_dir=/home/hadoop/edf
-hadoop_base_dir=/user/hadoop/edf
-
-spec_files_path=$base_dir/gwpl/spec_files
-jar_path=$base_dir/jars
-spark_app_jar_file_suffix=edf_dataingestion-assembly
-spark_app_jar_file_version=2.0.0.jar
+echo "Started the script for gwpl staging incremental load"
+source /home/hadoop/conf/env_conf.properties
 date=`date +%Y-%m-%d`
 date_ts=`date '+%Y-%m-%d_%H%M%S%s'`
-mkdir -p $base_dir/logs/$date
-log_file=$base_dir"/logs/"$date"/gwplstaging_"$date_ts".log"
+mkdir -p $ingestion_base_dir/logs/$date
+log_file=$ingestion_base_dir"/logs/"$date"/gwplStaging_"$date_ts".log"
+
+spec_files_path=$ingestion_base_dir/gwpl/spec_files
 
 spark-submit --class edf.dataload.DataIngestion \
- --master yarn --deploy-mode cluster  --conf spark.shuffle.spill=true \
- --conf spark.executor.extraJavaOptions=-XX:MaxPermSize=512m \
+ --master yarn --deploy-mode cluster  \
+ --conf $spark_common_conf \
  --conf spark.scheduler.mode=FAIR \
  --conf spark.scheduler.allocation.file=fairScheduler.xml \
- --conf spark.yarn.maxAppAttempts=1 \
- --conf spark.sql.planner.externalSort=true --conf spark.shuffle.manager=sort \
- --conf spark.ui.port=8088 --conf spark.executor.memoryOverhead=12096  \
- --conf spark.rpc.message.maxSize=1024 --conf spark.file.transferTo=false \
- --conf spark.driver.maxResultSize=10g --conf spark.rdd.compress=true \
- --conf spark.executor.extraJavaOptions="-Dconfig.resource=spark-defaults.conf" \
- --conf spark.driver.JavaOptions="-Dspark.yarn.app.container.log.dir=/mnt/var/log/hadoop" \
- --conf spark.driver.extraJavaOptions="-Dconfig.file=spark-defaults.conf" \
- --conf "spark.sql.parquet.writeLegacyFormat=true" \
- --conf spark.executor.memory=15g --conf spark.driver.memory=16g  \
+ --conf spark.executor.memory=15g --conf spark.driver.memory=15g  \
  --conf spark.executor.cores=5 \
  --conf spark.dynamicAllocation.enabled=true \
- --conf spark.dynamicAllocation.maxExecutors=60 \
- --conf spark.dynamicAllocation.minExecutors=1 \
- --conf spark.executor.instances=6 \
- --conf spark.serializer=org.apache.spark.serializer.KryoSerializer  \
+ --conf spark.dynamicAllocation.maxExecutors=50 \
+ --conf spark.dynamicAllocation.minExecutors=6 \
  --name "gwpl_di_staging_incremental"  \
   --files ${spec_files_path}/fairScheduler.xml#fairScheduler.xml,${spec_files_path}/gwpl_staging_DI.properties#diProperties.properties,${spec_files_path}/gwpl_lookup_info.csv#gwpl_lookup_info.csv,${spec_files_path}/gwpl_pii_spec.csv#gwpl_pii_spec.csv,${spec_files_path}/gwpl_table_spec.csv#gwpl_table_spec.csv,${spec_files_path}/gwpl_staging_list.csv#gwpl_staging_list.csv,/etc/spark/conf/hive-site.xml \
  --properties-file /usr/lib/spark/conf/spark-defaults.conf \
- ${jar_path}/${spark_app_jar_file_suffix}-${spark_app_jar_file_version} \
+ $ingestion_jar_path/$staging_incr_jar_name \
 gwpl_di_staging_incremental  1>>$log_file 2>&1
 status=$?
 echo "Completed the script and the status is $status"
--- config/PROD/pclte/scripts/pclte_staging_tl.sh
+++ config/PROD/pclte/scripts/pclte_staging_tl.sh
@@ -1,44 +1,24 @@
 #!/bin/bash
 
-
-echo "Started the script for PCLTE TL Staging "
-base_dir=/home/hadoop/processed
-hadoop_base_dir=/user/hadoop/processed
-
-spec_files_path=$base_dir/pclte/spec_files
-jar_path=$base_dir/jars
-spark_app_jar_file_suffix=edf_dataingestion-assembly
-spark_app_jar_file_version=2.1.0.jar
+echo "Started the script for pclte staging initial load"
+source /home/hadoop/conf/env_conf.properties
 date=`date +%Y-%m-%d`
 date_ts=`date '+%Y-%m-%d_%H%M%S%s'`
-mkdir -p $base_dir/logs/$date
-log_file=$base_dir"/logs/"$date"/pcltestaging_"$date_ts".log"
+mkdir -p $ingestion_base_dir/logs/$date
+log_file=$ingestion_base_dir"/logs/"$date"/pclteStaging_"$date_ts".log"
+spec_files_path=$ingestion_base_dir/pclte/spec_files
 
 spark-submit --class edf.dataload.DataIngestion \
- --master yarn --deploy-mode cluster  --conf spark.shuffle.spill=true \
- --conf spark.executor.extraJavaOptions=-XX:MaxPermSize=512m \
+ --master yarn --deploy-mode cluster  \
+ --conf $spark_common_conf \
  --conf spark.scheduler.mode=FAIR \
  --conf spark.scheduler.allocation.file=fairScheduler.xml \
- --conf spark.yarn.maxAppAttempts=1 \
- --conf spark.sql.planner.externalSort=true --conf spark.shuffle.manager=sort \
- --conf spark.ui.port=8088 --conf spark.executor.memoryOverhead=12096  \
- --conf spark.rpc.message.maxSize=1024 --conf spark.file.transferTo=false \
- --conf spark.driver.maxResultSize=10g --conf spark.rdd.compress=true \
- --conf spark.executor.extraJavaOptions="-Dconfig.resource=spark-defaults.conf" \
- --conf spark.driver.JavaOptions="-Dspark.yarn.app.container.log.dir=/mnt/var/log/hadoop" \
- --conf spark.driver.extraJavaOptions="-Dconfig.file=spark-defaults.conf" \
- --conf "spark.sql.parquet.writeLegacyFormat=true" \
- --conf spark.executor.memory=25g --conf spark.driver.memory=20g  \
- --conf spark.executor.cores=5 \
- --conf spark.dynamicAllocation.enabled=true \
- --conf spark.dynamicAllocation.maxExecutors=50 \
- --conf spark.dynamicAllocation.minExecutors=1 \
- --conf spark.executor.instances=6 \
- --conf spark.serializer=org.apache.spark.serializer.KryoSerializer  \
+ --conf spark.executor.memory=25g --conf spark.driver.memory=20g  --conf spark.executor.cores=5 \
+ --conf spark.dynamicAllocation.enabled=true --conf spark.dynamicAllocation.maxExecutors=50 --conf spark.dynamicAllocation.minExecutors=6 \
  --name "pclte_tl_staging_incremental"  \
   --files ${spec_files_path}/fairScheduler.xml#fairScheduler.xml,${spec_files_path}/pclte_staging_TL.properties#diProperties.properties,${spec_files_path}/pclte_lookup_info.csv#pclte_lookup_info.csv,${spec_files_path}/pclte_pii_spec.csv#pclte_pii_spec.csv,${spec_files_path}/pclte_table_spec.csv#pclte_table_spec.csv,${spec_files_path}/pclte_staging_list.csv#pclte_staging_list.csv,/etc/spark/conf/hive-site.xml \
  --properties-file /usr/lib/spark/conf/spark-defaults.conf \
- ${jar_path}/${spark_app_jar_file_suffix}-${spark_app_jar_file_version} \
+ $ingestion_jar_path/$staging_incr_jar_name \
 pclte_tl_staging_incremental  1>>$log_file 2>&1
 status=$?
 echo "Completed the script and the status is $status"
